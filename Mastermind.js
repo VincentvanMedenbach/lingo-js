4580b53b9913a301ef2874d351d0f3cd885cf146
Unknown <vincent1906@live.nl>
2017-11-29 11:11:04 +0100

    It works

---
+++ Mastermind.js
@@ -0,0 +1,148 @@
+var count = 0;
+var buttonActivate = document.getElementById("buttonActivate");
+var runOnce = false;
+randomWord();
+var totalPlacesOther = 0;
+var placesOther = 0;
+var placesRight = 0;
+var won = false;
+var isRight = [true, true, true, true, true, true, true];
+var isRightNumber = 0;
+var answer = "";
+var buttonCount = 5;
+var currentWord;
+
+function row() {
+    row.prototype.newRow = function () {
+        var buttonGroup = [document.createElement("input"), document.createElement("input"), document.createElement("input"), document.createElement("input"), document.createElement("input")
+        ];
+        for (var i = 0; i < buttonCount; i++) {
+            var k = i;
+            buttonGroup[k].type = "text";
+            buttonGroup[k].maxLength = 1;
+            var answerForBitch = currentWord.split("");
+            buttonGroup[0].value = answerForBitch[0];
+            buttonGroup[0].disabled = true;
+            buttonGroup[0].style.backgroundColor = "red";
+            buttonGroup[k].setAttribute("id", "button" + k + "_" + count);
+            container.appendChild(buttonGroup[k]);
+        }
+    };
+}
+
+var container = document.getElementById("container");
+
+function activation() {
+
+    answer = currentWord.split("");
+    checkers();
+    if (placesRight === buttonCount) {
+        victory();
+    }
+    isRight = [true, true, true, true, true, true, true, true];
+    totalPlacesOther = 0;
+    placesOther = 0;
+    placesRight = 0;
+    buttonDeactivate();
+    buttonActivate.innerHTML = "Activate";
+    count++;
+    current = "buttonRow" + count;
+
+
+    if (won === false) {
+        switch (count) {
+            case 0:
+                var current = new row();
+                current.newRow();
+                break;
+            case 1:
+                current = new row();
+                current.newRow();
+                break;
+            case 2:
+                current = new row();
+                current.newRow();
+                break;
+            case 3:
+                current = new row();
+                current.newRow();
+                break;
+            case 4:
+                current = new row();
+                current.newRow();
+                break;
+            case 5:
+                current = new row();
+                current.newRow();
+                break;
+            default:
+                buttonActivate.disabled = true;
+                break;
+
+        }
+    }
+    runOnce = true;
+
+}
+
+function buttonDeactivate() {
+    if (runOnce === true && won === false) {
+        for (var d = 0; d < buttonCount; d++) {
+            document.getElementById("button" + d + "_" + count).disabled = true;
+        }
+    }
+}
+
+function randomWord() {
+    var randomNummer = Math.floor((Math.random() * 470) + 1);
+    currentWord = words[randomNummer];
+    answer = currentWord.split("");
+    console.log(answer);
+}
+
+function checkers() {
+    if (runOnce == true) {
+        checkerProxy();
+        totalPlacesOther = buttonCount - (placesOther + placesRight);
+    }
+}
+
+function checkerProxy() {
+
+    for (var varCheck1 = 0; varCheck1 < buttonCount; varCheck1++) {
+        checkPlace(varCheck1);
+    }
+    for (var varCheck2 = 0; varCheck2 < buttonCount; varCheck2++) {
+        checkColors(varCheck2);
+    }
+}
+
+
+function checkPlace(Number) {
+    if (answer[Number] === document.getElementById("button" + Number + "_" + count).value) {
+        placesRight++;
+        isRightNumber = Number;
+        isRight[isRightNumber] = false;
+        answer[Number] = "";
+        document.getElementById("button" + Number + "_" + count).style.backgroundColor = "red";
+    }
+}
+
+function checkColors(Number) {
+    for (var i = 0; i < 800; i++) {
+        // if (i !== (Number)) {
+            if (answer[i] === document.getElementById("button" + Number + "_" + count).value && isRight[Number] === true) {
+                placesOther++;
+                answer[i] = "";
+                document.getElementById("button" + Number + "_" + count).setAttribute("style", "border-radius: 50%; background-Color: yellow;");
+            }
+        }
+
+    // }
+}
+
+function victory() {
+    won = true;
+    buttonActivate.disabled = true;
+    alert("you won!");
+}
--- README.md
+++ README.md
@@ -1,24 +1,33 @@
-## Lingo Javascript
-Tijdens deze opdracht ga je het spel Lingo programmeren in de web browser. Hierbij maak je gebruik van HTML, CSS en Javascript.
+## Mastermind Javascript
+Tijdens deze opdracht ga je het spel Mastermind programmeren in de web browser. Hierbij maak je gebruik van HTML, CSS en Javascript.
 
 De opdracht bestaat uit twee type eisen, functionele eisen en technische eisen. Je moet aan alle deze eisen voldoen!
 
-### Wat is Lingo
+### Wat is Mastermind
 
-![Voorbeeld](lingo.png)
+![Voorbeeld](https://upload.wikimedia.org/wikipedia/commons/thumb/2/2d/Mastermind.jpg/220px-Mastermind.jpg)
 
-Lingo is een woordspel dat een combinatie is van Mastermind en Bingo. Twee teams van twee mensen gaan met elkaar de strijd aan.
+Mastermind is een klassieker onder de spellen in logisch denken. De ene speler probeert de geheime code van de andere speler te ontrafelen.
 
-Het team dat aan de beurt is krijgt 5 pogingen om een woord te raden waarvan de beginletter gegeven is. De lange Ĳ telt als één letter. Het team mag niet overleggen. Bij elke poging wordt aangegeven welke letters op de juiste plaats staan (rood vakje) en welke letters in het woord zitten maar op een verkeerde plaats (gele cirkel). Bij overschrijding van de bedenktijd mag het andere team deze poging spelen met een bonusletter (een extra letter die op de juiste plaats wordt gezet).
+De codemaker kiest in het geheim vier code-pionnen en plaatst deze in een gekozen volgorde achter het zichtschermpje. Het is toegelaten om twee of meer pionnen in dezelfde kleur te kiezen. De codemaker heeft de keuze uit zes verschillende kleuren. Dit is de verborgen code die de codebreker moet gaan ontrafelen.
+
+In zijn beurt plaatst de codebreker vier code-pionnen naar keuze in de eerste lege rij. Nu geeft de codemaker met de sleutel-pinnen het resultaat:
+
+* met rode/zwarte pinnen: hoeveel kleuren staan op de juiste positie?
+* met witte pinnen: hoeveel kleuren komen wel in de code voor, maar staan niet op de juiste positie?
+
+Met deze nieuwe informatie plaatst de codebreker vier volgende code-pionnen in de eerstvolgende lege rij.
 
 ### Functionele eisen
 Functionele eisen beschrijven welke functionaliteit je eindproduct moet bevatten. Functionele eisen mogen nooit van technische aard zijn! Ieder persoon moet begrijpen welke functionaliteit de opdracht moet bevatten, ongeacht kennisniveau.
 
 Het spel moet aan de volgende functionele eisen voldoen:
-* Het spel (de code) selecteert willekeurig een woord uit de woordenlijst wat door de speler moet worden geraden.
-* Het spel toont de beginletter van het woord.
-* De speler krijgt 5 pogingen om het woord te raden.
-* Bij elke poging worden de letters die op de juiste plaats met een rood vierkant getoont. Letters die wel in het woord zitten maar op de verkeerde plaats staan worden met een gele cirkel getoont.
+* De speler speelt de rol van de codebreker. 
+* De computer (de code) speelt de rol van de codemaker.
+* Ieder spel bestaat uit 12 rijen.
+* Iedere rij bestaat uit 4 plaatsen.
+* Het spel wordt gespeelt met 6 verschillende kleuren.
+* Bij het resultaat van een rij worden de kleuren rood en wit gebruikt. Rood geeft aan hoeveel kleuren op de juiste positie staan. Met wit wordt aangegeven dat een kleur wel in de code voor komt, maar niet op de juiste positie staat.
 
 ### Technische eisen
 Technische eisen beschrijven aan welke vakgerichte voorwaarden de opdracht moet voldoen. Bijvoorbeeld dat de opdracht in correct validerend HTML en CSS moet zijn geschreven. 
@@ -26,8 +35,8 @@ Technische eisen beschrijven aan welke vakgerichte voorwaarden de opdracht moet 
 Het spel moet aan de volgende technische eisen voldoen:
 * De presentatie van het spel is gemaakt door gebruik te maken van HTML en CSS.
 * De logica van het spel is geprogrammeert in Javascript.
-* De woordenlijst (lingo-nl.js) wordt ingeladen doormiddel van \<script\> tags.
-* Het spel selecteert willekeurig (random) een woord uit de woordenlijst. 
+* Het spel genereert willekeurig (random) een kleurencode. 
 
 ### Bonus
 * Het gehele spel is gemaakt door gebruik te maken van javascript objecten.
+* Bij de start van een spel kan de speler kiezen hoeveel plaatsen en verschillende kleuren gebruikt worden.
